35b7c36d9b6571f380a62917216cb465e9068f5a
Layne Penney <layne@evove.tech>
2017-12-04 13:20:19 -0600

    kotlin fixes

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
-    kotlin("android")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
     kotlin("kapt")
 }
 
@@ -23,11 +24,11 @@ android {
     }
 }
 
-val app = extra["app"]
+val app: groovy.util.Expando by extra
+val kotlinVersion = app.getProperty("kotlinVersion")!!
 
 dependencies {
-    //    implementation(fileTree(dir = "libs", include = ["*.jar"]))
-//    implementation("org.jetbrains.kotlin:kotlin-stdlib-jre7:${app.getProperty("kotlinVersion")}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jre7:${kotlinVersion}")
     implementation("com.android.support:appcompat-v7:26.1.0")
     implementation("com.android.support:design:26.1.0")
     implementation("com.android.support.constraint:constraint-layout:1.0.2")
